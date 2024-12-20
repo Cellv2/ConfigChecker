140c442dea7abdf8b5a9cfadd19dcfa83d211767
Cellv2 <22135118+Cellv2@users.noreply.github.com>
2024-12-20 14:41:43 +0000

    Unaccept namespace indenting

--- ConfigChecker.Agent/Services/ConfigProcessor/ConfigToObjectMappers/JsonConfigMapper.cs
+++ ConfigChecker.Agent/Services/ConfigProcessor/ConfigToObjectMappers/JsonConfigMapper.cs
@@ -1,24 +1,23 @@
 ﻿using ConfigChecker.Agent.Constants;
 using System.Text.Json;
 
-namespace ConfigChecker.Agent.Services.ConfigProcessor.ConfigToObjectMappers
+namespace ConfigChecker.Agent.Services.ConfigProcessor.ConfigToObjectMappers;
+
+public sealed class JsonConfigMapper : IConfigToObjectMapperBase
 {
-    public sealed class JsonConfigMapper : IConfigToObjectMapperBase
+    public string[] ConfigsToProcess => ConfigSearch.JsonConfigFileNamesToSearch;
+
+    public object? MapFileToObject(string filePath)
     {
-        public string[] ConfigsToProcess => ConfigSearch.JsonConfigFileNamesToSearch;
-
-        public object? MapFileToObject(string filePath)
+        var fileName = Path.GetFileName(filePath);
+        if (!ConfigsToProcess.Contains(fileName))
         {
-            var fileName = Path.GetFileName(filePath);
-            if (!ConfigsToProcess.Contains(fileName))
-            {
-                return null;
-            }
-
-            var fileContent = File.ReadAllText(filePath);
-            var deserialized = JsonSerializer.Deserialize<object>(fileContent);
-
-            return deserialized;
+            return null;
         }
+
+        var fileContent = File.ReadAllText(filePath);
+        var deserialized = JsonSerializer.Deserialize<object>(fileContent);
+
+        return deserialized;
     }
 }
